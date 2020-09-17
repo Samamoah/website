ac9718b87d23d15143792e6bae298c122c8acad6
Samuel Tetteh <samuelamoahtetteh@gmail.com>
2020-09-17 01:47:09 +0000

    gh-pages

---
+++ precache-manifest.faca8300bcb54c647e703398874be0dd.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e72c669b7746f14d9939",
+    "url": "/css/app.b36f7d9a.css"
+  },
+  {
+    "revision": "aed0d5091c1445c09e2ba77c0577c03a",
+    "url": "/img/bg-bottom.aed0d509.svg"
+  },
+  {
+    "revision": "c52a4e7b753e927c3ad436eb17fe6706",
+    "url": "/img/bg-md.c52a4e7b.svg"
+  },
+  {
+    "revision": "486e21d73dc6e1a03712875873523143",
+    "url": "/img/bg-top.486e21d7.png"
+  },
+  {
+    "revision": "966f889f0d8a4e55ff6cdad920bac153",
+    "url": "/img/clapper.966f889f.jpg"
+  },
+  {
+    "revision": "323cb0d99c823165911eb8994214bb2d",
+    "url": "/img/logo.323cb0d9.png"
+  },
+  {
+    "revision": "b6acb15c3144a619fd5f9f882cbcd455",
+    "url": "/img/prediction.b6acb15c.jpg"
+  },
+  {
+    "revision": "13f05f863a9e32759259a1b0d99c5044",
+    "url": "/img/zcoutz.13f05f86.jpg"
+  },
+  {
+    "revision": "e05d0480acf798912fb3d0dcc5db7232",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e72c669b7746f14d9939",
+    "url": "/js/app.009f8372.js"
+  },
+  {
+    "revision": "903942d5d09578fd295f",
+    "url": "/js/chunk-vendors.406ef2cf.js"
+  },
+  {
+    "revision": "1d8900db09706b08da41608769860343",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/robots.txt"
+  }
+]);
